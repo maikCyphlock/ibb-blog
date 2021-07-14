09430fcf8b901f6927fa4f589935b25ea44eccc2
maikol aguilar <66704744+maikCyphlock@users.noreply.github.com>
2021-07-14 07:42:55 -0400

    change lang and name instead of dev blog

--- components/Post.js
+++ components/Post.js
@@ -5,14 +5,14 @@ export default function Post({ post }) {
     <div className='card'>
       <img src={post.frontmatter.cover_image} alt='' />
 
-      <div className='post-date'>Posted on {post.frontmatter.date}</div>
+      <div className='post-date'>posteado el {post.frontmatter.date}</div>
 
       <h3>{post.frontmatter.title}</h3>
 
       <p>{post.frontmatter.excerpt}</p>
 
       <Link href={`/blog/${post.slug}`}>
-        <a className='btn'>Read More</a>
+        <a className='btn'>leer más</a>
       </Link>
     </div>
   )
